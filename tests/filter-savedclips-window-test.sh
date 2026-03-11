6bc7c1aa0863fe42a858509dd786302fc094f9ad
Andy Lin <andyylin@users.noreply.github.com>
2026-03-11 22:59:26 +0800

    Avoid reprocessing filtered SavedClips clips

--- tests/filter-savedclips-window-test.sh
+++ tests/filter-savedclips-window-test.sh
@@ -5,6 +5,8 @@ SCRIPT="$(cd "$(dirname "$0")/.." && pwd)/run/filter_savedclips_window.py"
 TMPDIR="$(mktemp -d)"
 LIST="$TMPDIR/sentry_files"
 EXP="$TMPDIR/expected"
+REMOVED="$TMPDIR/removed"
+EXP_REMOVED="$TMPDIR/expected_removed"
 
 cleanup() {
   rm -rf "$TMPDIR"
@@ -21,7 +23,7 @@ SavedClips/2026-01-01_13-00-00/2026-01-01_13-02-00-front.mp4
 SentryClips/2026-01-01_12-00-00/2026-01-01_12-00-00-front.mp4
 DATA
 
-python3 "$SCRIPT" --list "$LIST" --minutes 3
+python3 "$SCRIPT" --list "$LIST" --minutes 3 --removed-list "$REMOVED"
 
 cat > "$EXP" <<DATA
 SavedClips/2026-01-01_12-00-00/2026-01-01_12-05-00-front.mp4
@@ -31,6 +33,12 @@ SavedClips/2026-01-01_13-00-00/2026-01-01_13-02-00-front.mp4
 SentryClips/2026-01-01_12-00-00/2026-01-01_12-00-00-front.mp4
 DATA
 
+cat > "$EXP_REMOVED" <<DATA
+SavedClips/2026-01-01_12-00-00/2026-01-01_12-00-00-front.mp4
+SavedClips/2026-01-01_12-00-00/2026-01-01_12-01-00-front.mp4
+DATA
+
 diff -u "$EXP" "$LIST"
+diff -u "$EXP_REMOVED" "$REMOVED"
 
 echo "filter-savedclips-window-test: OK"
